d3b37e892b2723327f05e5c1742d509fbe91ab86
Umar Farouq <umarfarouq447@gmail.com>
2024-10-12 16:56:55 +0100

    Rendering successful

--- script.js
+++ script.js
@@ -9,4 +9,25 @@ const fetchData = async () => {
     } catch (error) {
         console.error("Error fetching Meals: ", error)
     }
-}
+};
+
+const displayMeals = (data) => {
+    const mealList = document.getElementById("meal_list");
+    data.meals.forEach(meal => {
+        mealList.innerHTML += `
+         <div class="meal" id="meal">
+                <div class="img"><img src="${meal.strMealThumb}" alt="Meal Image"></div>
+                <h2>${meal.strMeal}</h2>
+                <p>${meal.strInstructions}</p>
+                <p>ing 1</p>
+                <p>ing 2</p>
+                <p>ing 3</p>
+                <p>ing 4</p>
+                <p>ing 5</p>
+                <button>Watch Video</button>
+            </div>
+        `
+    });
+};
+
+fetchData();
